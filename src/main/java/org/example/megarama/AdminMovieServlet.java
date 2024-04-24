d1a3dc8844eb4b98a479406f8a54460a635e2f84
iima23 <elmazouz.imane@gmail.com>
2024-04-24 09:12:53 +0100

    add movie class

---
+++ src/main/java/org/example/megarama/AdminMovieServlet.java
@@ -0,0 +1,12 @@
+package org.example.megarama;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Time;
+
+public class AdminMovieServlet {
+
+    }
