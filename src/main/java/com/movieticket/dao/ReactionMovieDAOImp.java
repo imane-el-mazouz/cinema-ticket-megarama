a4085d72b373279d166af45ed6b2f0a676261ddf
iima23 <elmazouz.imane@gmail.com>
2024-05-03 15:35:36 +0100

    add function AddFunction

--- src/main/java/com/movieticket/dao/ReactionMovieDAOImp.java
+++ src/main/java/com/movieticket/dao/ReactionMovieDAOImp.java
@@ -1,8 +1,15 @@
 package com.movieticket.dao;
 
 import com.movieticket.model.ReactionMovie;
+import com.Connection.DatabaseManager;
 
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+
+import static com.Connection.DatabaseManager.getConnection;
 
 public class ReactionMovieDAOImp implements ReactionMovieDAO {
     @Override
@@ -22,8 +29,23 @@ public class ReactionMovieDAOImp implements ReactionMovieDAO {
 
     @Override
     public void addReactionMovie(ReactionMovie reactionMovie) {
+        String query = "INSERT INTO reaction_movies (reactionMovieId ,user_id, movie_id, rating, comment) VALUES (?, ?, ?, ? ,?)";
 
+
+            try (Connection connection = getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                preparedStatement.setInt(1, reactionMovie.getReactionMovieId());
+                preparedStatement.setInt(2, reactionMovie.getUserId());
+                preparedStatement.setInt(3, reactionMovie.getMovieId());
+                preparedStatement.setInt(4, reactionMovie.getRating());
+                preparedStatement.setString(5, reactionMovie.getComment());
+
+                preparedStatement.executeUpdate();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
     }
+
 
     @Override
     public void updateReactionMovie(ReactionMovie reactionMovie) {
